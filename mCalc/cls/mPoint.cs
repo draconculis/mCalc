a73160e6626d6a25fa149613b680713b3f561758
timopedersen <draconculis@hotmail.com>
2018-03-22 18:08:17 +0100

    Initial

---
+++ mCalc/cls/mPoint.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mCalc.cls
+{
+    public class mPoint : IComparable
+    {
+        public long Ticks { get; set; }
+        public Decimal Amount { get; set; }
+
+        public DateTime Date
+        {
+            get => new DateTime(Ticks);
+            set => Ticks = value.Ticks;
+        }
+
+        #region Contructors -------------------------------------------
+        public mPoint(decimal amount)
+            : this(DateTime.Now.Ticks, amount)
+        {
+        }
+
+        public mPoint(long ticks, decimal amount)
+        {
+            Ticks = ticks;
+            Amount = amount;
+        }
+
+        public mPoint(DateTime date, decimal amount)
+        {
+            Date = date;
+            Amount = amount;
+        }
+        #endregion Contructors ----------------------------------------
+
+        #region ICompare stuff ----------------------------------------
+        public int CompareTo(object obj)
+        {
+            return this.Ticks.CompareTo(((mPoint)obj).Ticks);
+        }
+
+        public class AmountComparer : IComparer<mPoint>
+        {
+            public int Compare(object x, object y)
+            {
+                return ((mPoint)x).Amount.CompareTo(((mPoint)y).Amount);
+            }
+
+            public int Compare(mPoint x, mPoint y)
+            {
+                return x.Amount.CompareTo(y.Amount);
+            }
+        }
+        #endregion ICompare stuff -------------------------------------
+    }
+
+}
---
+++ mCalc/ext/IEnumeratorExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dek572.Extensions
+{
+    public static class IEnumeratorExtensions
+    {
+
+        /// <summary>
+        /// Merge ordered lists contained in a list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TOrder"></typeparam>
+        /// <param name="aa"></param>
+        /// <param name="orderFunc"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> MergePreserveOrder<T, TOrder>(
+            this IEnumerable<IEnumerable<T>> aa,
+            Func<T, TOrder> orderFunc) where TOrder : IComparable<TOrder>
+        {
+            List<Tuple<TOrder, IEnumerator<T>>> items = aa.Select(xx => xx.GetEnumerator())
+                .Where(ee => ee.MoveNext())
+                .Select(ee => Tuple.Create(orderFunc(ee.Current), ee))
+                .OrderBy(ee => ee.Item1).ToList();
+
+            while (items.Count > 0)
+            {
+                yield return items[0].Item2.Current;
+
+                var next = items[0];
+                items.RemoveAt(0);
+                if (next.Item2.MoveNext())
+                {
+                    var value = orderFunc(next.Item2.Current);
+                    var ii = 0;
+                    for (; ii < items.Count; ++ii)
+                    {
+                        if (value.CompareTo(items[ii].Item1) <= 0)
+                        {
+                            // NB: using a tuple to minimize calls to orderFunc
+                            items.Insert(ii, Tuple.Create(value, next.Item2));
+                            break;
+                        }
+                    }
+
+                    if (ii == items.Count) items.Add(Tuple.Create(value, next.Item2));
+                }
+                else next.Item2.Dispose(); // Dispose enumerator
+            }
+        }
+
+        /// <summary>
+        /// Merge 2 ordered lists
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TOrder"></typeparam>
+        /// <param name="aa"></param>
+        /// <param name="bb"></param>
+        /// <param name="orderFunc"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> MergePreserveOrder<T, TOrder>(
+            this IEnumerable<T> aa,
+            IEnumerable<T> bb,
+            Func<T, TOrder> orderFunc) 
+            where TOrder : IComparable<TOrder>
+        {
+            return MergePreserveOrder(new List<IEnumerable<T>>{aa, bb}, orderFunc);
+        }
+    }
+}
